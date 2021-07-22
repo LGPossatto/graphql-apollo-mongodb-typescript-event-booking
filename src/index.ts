7134a34138c7d6637ffbea3a9d721b045e3c2db8
Luiz Gustavo <lgpossatto@gmail.com>
2021-07-22 17:46:13 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,20 @@ import schema from "./graphql/schema";
 (async () => {
   const app = express();
   app.use(express.json());
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
   app.use(
     "/graphql",
     graphqlHTTP(async (req) => {
